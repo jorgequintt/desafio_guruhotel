ede79960f6825312e40ad590e9f436c47e2976f2
jq <jorgequintt@gmail.com>
2021-01-29 17:50:11 -0500

    Endpoint /business/:id working correctly

--- pages/api/business/[id].js
+++ pages/api/business/[id].js
@@ -1,24 +1,40 @@
-export default function handler(req, res) {
+import yelpGqlFetch from '../../../lib/yelpGqlFetch';
+
+export default async function handler(req, res) {
    if (req.method === 'GET') {
       const { id } = req.query;
 
       if (!id) res.status(400).json({ error: 'You must specify an id' });
 
-      const gqlQuery = `#graphql
-      {
-        business(id: $businessId) {
-            price
-            hours
-            is_closed
-            reviews {
-                rating
-                text
-                user {
-                    name
+      const query = `#graphql
+        query ($id: String){
+            business(id: $id) {
+                price
+                hours {
+                    is_open_now
+                    open {
+                        end
+                        start
+                    }
+                }
+                is_closed
+                reviews {
+                    rating
+                    text
+                    user {
+                        name
+                    }
                 }
             }
-        }
-     }`;
+        }`;
+
+      // request to Yelp API
+      const response = await yelpGqlFetch(query, { id });
+      if (response.error) {
+         return res.status(response.status).json(response);
+      } else {
+         res.status(200).json(response);
+      }
    } else {
       res.status(400).json({ error: 'Not a valid request method' });
    }
